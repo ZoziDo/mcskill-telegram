d4cf81bc60f31000bd9da1fb28787aa4d23926d9
ZoziDo <163629061+ZoziDo@users.noreply.github.com>
2026-05-20 14:38:23 +0700

    Update botproxy.py

--- botproxy.py
+++ botproxy.py
@@ -8,19 +8,22 @@ TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 KEY = os.getenv("KEY")
 
+@app.route("/")
+def home():
+    return "online"
+
 @app.route("/send")
 def send():
     if request.args.get("key") != KEY:
-        return "denied"
+        return "denied", 403
 
     msg = request.args.get("msg", "")
+    if not msg:
+        return "empty"
 
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={
-            "chat_id": CHAT_ID,
-            "text": msg
-        }
+        data={"chat_id": CHAT_ID, "text": msg}
     )
 
     return "ok"
